38415e5fb3532c79ac82feaa070f4045957d85a1
Tosin Ayanleye <100681205+Tosin0814@users.noreply.github.com>
2022-07-16 17:34:52 -0400

    Seperate questions

---
+++ js/questions.js
@@ -0,0 +1,179 @@
+//Array of Question Objects
+const trivia = [
+    {
+      prompt: "Who played Mrs. Robinson in The Graduate?",
+      optA:"Katherine Rose",
+      optB:"Anne Bancroft",
+      optC:"Elizabeth Wilson",
+      optD:"Oprah Winfrey",
+      answer: "Anne Bancroft"
+    },
+    {
+      prompt: "What was the first feature-length animated movie ever released?",
+      optA:"Shrek",
+      optB:"The Little Mermaid",
+      optC:"Snow White and the Seven Dwarfs",
+      optD:"Cinderella",
+      answer: "Snow White and the Seven Dwarfs"
+    },
+    {
+      prompt: "In The Matrix, does Neo take the blue pill or the red pill?",
+      optA:"Blue",
+      optB:"Red",
+      optC:"Both",
+      optD:"Neither",
+      answer: "Red"
+    },
+    {
+      prompt: "What's the name of the skyscraper in Die Hard?",
+      optA:"Empire State Building",
+      optB:"Central Park Tower",
+      optC:"Devon Tower",
+      optD:"Nakatomi Plaza",
+      answer: "Nakatomi Plaza"
+    },
+    {
+      prompt: "What flavor of Pop Tarts does Buddy the Elf use in his spaghetti in Elf? ",
+      optA:"Peanut",
+      optB:"Strawberry",
+      optC:"Chocolate",
+      optD:"Vanilla",
+      answer: "Chocolate"
+    },
+    {
+      prompt: "The head of what kind of animal is front-and-center in an infamous scene from The Godfather?",
+      optA:"A lion",
+      optB:"A dog",
+      optC:"A horse",
+      optD:"A bull",
+      answer: "A horse"
+    },
+    {
+      prompt: "What famous L.A. landmark is heavily featured in Rebel Without a Cause?",
+      optA:"Staples Center",
+      optB:"Venice Canals Walkway",
+      optC:"Hollywood Walk of Fame",
+      optD:"Griffith Observatory",
+      answer: "Griffith Observatory"
+    },
+    {
+      prompt: "Who played Martin Luther King Jr. in the 2014 biopic Selma?",
+      optA:"David Oyelowo",
+      optB:"Sidney Poitier",
+      optC:"Morgan Freeman",
+      optD:"Denzel Washington",
+      answer: "David Oyelowo"
+    },
+    {
+      prompt: "What Hollywood movie star plays himself in Zombieland?",
+      optA:"Indiana Jones",
+      optB:"Woody Harrelson",
+      optC:"Emma Stone",
+      optD:"Bill Murray",
+      answer: "Bill Murray"
+    },
+    {
+      prompt: "What is the highest-grossing R-rated movie of all time?",
+      optA:"The Matrix Reloaded",
+      optB:"Joker",
+      optC:"Deadpool",
+      optD:"Deadpool 2",
+      answer: "Joker"
+    },
+    {
+      prompt: "What 1994 crime film revitalized John Travolta's career?",
+      optA:"Perfect",
+      optB:"From Paris with Love",
+      optC:"The poison Rose",
+      optD:"Pulp Fiction",
+      answer: "Pulp Fiction"
+    },
+    {
+      prompt: "What animated classic was the first film of the late-twentieth-century 'Disney Renaissance?'",
+      optA:"Sleeping Beauty",
+      optB:"Cinderella",
+      optC:"The Little Mermaid",
+      optD:"Snow White",
+      answer: "The Little Mermaid"
+    },
+    {
+        prompt: "Aaron Sorkin won an Oscar for writing what 2010 drama about the creation of Facebook?",
+        optA:"The Social Network",
+        optB:"The Rise of an Era",
+        optC:"Going Public",
+        optD:"The Facebook",
+        answer: "The Social Network"
+    },
+    {
+        prompt: "Who is the first actor to play Jack Ryan on screen?",
+        optA:"Harrison Ford",
+        optB:"Alec Baldwin",
+        optC:"Ben Affleck",
+        optD:"Chris Pine",
+        answer: "Alec Baldwin"
+    },
+    {
+        prompt: "Joaquin Phoenix received his first Oscar nomination for playing Roman emperor Commodus in what 2000 Oscar-winning epic?",
+        optA:"Spartacus",
+        optB:"The Eagle",
+        optC:"Gladiator",
+        optD:"The Last Legion",
+        answer: "Gladiator"
+    },
+    {
+        prompt: "The Battle of Thermopylae served as the basis of what highly stylized 2006 smash hit swords-and-sandals action flick?",
+        optA:"The Legend of Hercules",
+        optB:"Pompei",
+        optC:"Spartacus",
+        optD:"300",
+        answer: "300"
+    },
+    {
+        prompt: "What is the highest-grossing foreign-language film at the U.S. box office?",
+        optA:"The Passion of the Christ",
+        optB:"Hero",
+        optC:"Crouching Tiger, Hidden Dragon",
+        optD:"Parasite",
+        answer: "The Passion of the Christ"
+    },
+    {
+        prompt: "Who wrote the screenplay for Rocky?",
+        optA:"Dwayne Johnson",
+        optB:"Sylvester Stallone",
+        optC:"Quentin Tarantino",
+        optD:"Spike Lee",
+        answer: "Sylvester Stallone"
+    },
+    {
+        prompt: "What was Quentin Tarantino's first feature as writer/director?",
+        optA:"Kill Bill",
+        optB:"Once Upon a Time in Hollywood",
+        optC:"Reservoir Dogs",
+        optD:"From Dusk Till Dawn",
+        answer: "Reservoir Dogs"
+    },
+    {
+        prompt: "Who played the Wicked Witch of the West in The Wizard of Oz?",
+        optA:"Angelina Jolie",
+        optB:"Jennifer Lopez",
+        optC:"Amber Heard",
+        optD:"Margaret Hamilton",
+        answer: "Margaret Hamilton"
+    },
+    {
+        prompt: "What Martin Scorsese movie holds the all-time record for F-bombs?",
+        optA:"The Wolf of Wall Street",
+        optB:"Alpha Dog",
+        optC:"Straight Outta Compton",
+        optD:"Casino",
+        answer: "The Wolf of Wall Street"
+    },
+    {
+        prompt: "Who played the Green Goblin in 2002 box-office smash Spider-Man?",
+        optA:"Steve Blum",
+        optB:"Willem Dafoe",
+        optC:"Jorma Taccone",
+        optD:"Rino Romano",
+        answer: "Willem Dafoe"
+    },
+];
