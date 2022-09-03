9c60b66af7eec2ccb84005ff57a21ca020418765
Tosin Ayanleye <100681205+Tosin0814@users.noreply.github.com>
2022-09-03 08:28:13 -0400

    Change questions to Beyonce trivia

--- js/questions.js
+++ js/questions.js
@@ -1,179 +1,195 @@
 //Array of Question Objects
 const trivia = [
-    {
-      prompt: "Who played Mrs. Robinson in The Graduate?",
-      optA:"Katherine Rose",
-      optB:"Anne Bancroft",
-      optC:"Elizabeth Wilson",
-      optD:"Oprah Winfrey",
-      answer: "Anne Bancroft"
-    },
-    {
-      prompt: "What was the first feature-length animated movie ever released?",
-      optA:"Shrek",
-      optB:"The Little Mermaid",
-      optC:"Snow White and the Seven Dwarfs",
-      optD:"Cinderella",
-      answer: "Snow White and the Seven Dwarfs"
-    },
-    {
-      prompt: "In The Matrix, does Neo take the blue pill or the red pill?",
-      optA:"Blue",
-      optB:"Red",
-      optC:"Both",
-      optD:"Neither",
-      answer: "Red"
-    },
-    {
-      prompt: "What's the name of the skyscraper in Die Hard?",
-      optA:"Empire State Building",
-      optB:"Central Park Tower",
-      optC:"Devon Tower",
-      optD:"Nakatomi Plaza",
-      answer: "Nakatomi Plaza"
-    },
-    {
-      prompt: "What flavor of Pop Tarts does Buddy the Elf use in his spaghetti in Elf? ",
-      optA:"Peanut",
-      optB:"Strawberry",
-      optC:"Chocolate",
-      optD:"Vanilla",
-      answer: "Chocolate"
-    },
-    {
-      prompt: "The head of what kind of animal is front-and-center in an infamous scene from The Godfather?",
-      optA:"A lion",
-      optB:"A dog",
-      optC:"A horse",
-      optD:"A bull",
-      answer: "A horse"
-    },
-    {
-      prompt: "What famous L.A. landmark is heavily featured in Rebel Without a Cause?",
-      optA:"Staples Center",
-      optB:"Venice Canals Walkway",
-      optC:"Hollywood Walk of Fame",
-      optD:"Griffith Observatory",
-      answer: "Griffith Observatory"
-    },
-    {
-      prompt: "Who played Martin Luther King Jr. in the 2014 biopic Selma?",
-      optA:"David Oyelowo",
-      optB:"Sidney Poitier",
-      optC:"Morgan Freeman",
-      optD:"Denzel Washington",
-      answer: "David Oyelowo"
-    },
-    {
-      prompt: "What Hollywood movie star plays himself in Zombieland?",
-      optA:"Indiana Jones",
-      optB:"Woody Harrelson",
-      optC:"Emma Stone",
-      optD:"Bill Murray",
-      answer: "Bill Murray"
-    },
-    {
-      prompt: "What is the highest-grossing R-rated movie of all time?",
-      optA:"The Matrix Reloaded",
-      optB:"Joker",
-      optC:"Deadpool",
-      optD:"Deadpool 2",
-      answer: "Joker"
-    },
-    {
-      prompt: "What 1994 crime film revitalized John Travolta's career?",
-      optA:"Perfect",
-      optB:"From Paris with Love",
-      optC:"The Poison Rose",
-      optD:"Pulp Fiction",
-      answer: "Pulp Fiction"
-    },
-    {
-      prompt: "What animated classic was the first film of the late-twentieth-century 'Disney Renaissance?'",
-      optA:"Sleeping Beauty",
-      optB:"Cinderella",
-      optC:"The Little Mermaid",
-      optD:"Snow White",
-      answer: "The Little Mermaid"
-    },
-    {
-        prompt: "Aaron Sorkin won an Oscar for writing what 2010 drama about the creation of Facebook?",
-        optA:"The Social Network",
-        optB:"The Rise of an Era",
-        optC:"Going Public",
-        optD:"The Facebook",
-        answer: "The Social Network"
-    },
-    {
-        prompt: "Who is the first actor to play Jack Ryan on screen?",
-        optA:"Harrison Ford",
-        optB:"Alec Baldwin",
-        optC:"Ben Affleck",
-        optD:"Chris Pine",
-        answer: "Alec Baldwin"
-    },
-    {
-        prompt: "Joaquin Phoenix received his first Oscar nomination for playing Roman emperor Commodus in what 2000 Oscar-winning epic?",
-        optA:"Spartacus",
-        optB:"The Eagle",
-        optC:"Gladiator",
-        optD:"The Last Legion",
-        answer: "Gladiator"
-    },
-    {
-        prompt: "The Battle of Thermopylae served as the basis of what highly stylized 2006 smash hit swords-and-sandals action flick?",
-        optA:"The Legend of Hercules",
-        optB:"Pompei",
-        optC:"Spartacus",
-        optD:"300",
-        answer: "300"
-    },
-    {
-        prompt: "What is the highest-grossing foreign-language film at the U.S. box office?",
-        optA:"The Passion of the Christ",
-        optB:"Hero",
-        optC:"Crouching Tiger, Hidden Dragon",
-        optD:"Parasite",
-        answer: "The Passion of the Christ"
-    },
-    {
-        prompt: "Who wrote the screenplay for Rocky?",
-        optA:"Dwayne Johnson",
-        optB:"Sylvester Stallone",
-        optC:"Quentin Tarantino",
-        optD:"Spike Lee",
-        answer: "Sylvester Stallone"
-    },
-    {
-        prompt: "What was Quentin Tarantino's first feature as writer/director?",
-        optA:"Kill Bill",
-        optB:"Once Upon a Time in Hollywood",
-        optC:"Reservoir Dogs",
-        optD:"From Dusk Till Dawn",
-        answer: "Reservoir Dogs"
-    },
-    {
-        prompt: "Who played the Wicked Witch of the West in The Wizard of Oz?",
-        optA:"Angelina Jolie",
-        optB:"Jennifer Lopez",
-        optC:"Amber Heard",
-        optD:"Margaret Hamilton",
-        answer: "Margaret Hamilton"
-    },
-    {
-        prompt: "What Martin Scorsese movie holds the all-time record for F-bombs?",
-        optA:"The Wolf of Wall Street",
-        optB:"Alpha Dog",
-        optC:"Straight Outta Compton",
-        optD:"Casino",
-        answer: "The Wolf of Wall Street"
-    },
-    {
-        prompt: "Who played the Green Goblin in 2002 box-office smash Spider-Man?",
-        optA:"Steve Blum",
-        optB:"Willem Dafoe",
-        optC:"Jorma Taccone",
-        optD:"Rino Romano",
-        answer: "Willem Dafoe"
-    },
+  {
+    prompt: "What is Beyoncé's middle name?",
+    optA:"CELESTINE",
+    optB:"GISELLE",
+    optC:"SOLANGE",
+    optD:"DESIREE",
+    answer: "GISELLE"
+  },
+  {
+    prompt: "Beyoncé rose to fame in the late 1990s as the lead singer of what R&B girl-group?",
+    optA:"TLC",
+    optB:"DESTINY'S CHILD",
+    optC:"EN VOGUE",
+    optD:"THE SPICE GIRLS",
+    answer: "DESTINY'S CHILD"
+  },
+  {
+    prompt: "Beyoncé made her big screen debut in what movie?",
+    optA:"THE PINK PANTHER",
+    optB:"ZOOLANDER",
+    optC:"DREAMGIRLS",
+    optD:"AUSTIN POWERS IN GOLDMEMBER",
+    answer: "AUSTIN POWERS IN GOLDMEMBER"
+  },
+  {
+    prompt: "What was the name of Beyoncé's solo debut album?",
+    optA:"I AM ... SASHA FIERCE",
+    optB:"DANGEROUSLY IN LOVE",
+    optC:"SURVIVOR",
+    optD:"B'DAY",
+    answer: "DANGEROUSLY IN LOVE"
+  },
+  {
+    prompt: "In what song does Beyoncé sing: 'I swore I'd never fall again, but this don't even feel like falling'?",
+    optA:"BROKEN-HEARTED GIRL",
+    optB:"HALO",
+    optC:"CRAZY IN LOVE",
+    optD:"ALL NIGHT",
+    answer: "HALO"
+  },
+  {
+    prompt: "What is the name of Beyoncé's all-female tour band?",
+    optA:"SUGA MAMA",
+    optB:"HEARTBREAKERS",
+    optC:"SWEET THINGS",
+    optD:"PARLIAMENT FUNKADELIC",
+    answer: "SUGA MAMA"
+  },
+  {
+    prompt: "Who did Beyoncé marry in 2008?",
+    optA:"KANYE WEST",
+    optB:"SEAN COMBS",
+    optC:"JAY-Z",
+    optD:"ICE CUBE",
+    answer: "JAY-Z"
+  },
+  {
+    prompt: "Destiny's Child released their major label debut song on the soundtrack of what film?",
+    optA:"I, ROBOT",
+    optB:"ENEMY OF THE STATE",
+    optC:"BAD BOYS",
+    optD:"MEN IN BLACK",
+    answer: "MEN IN BLACK"
+  },
+  {
+    prompt: "Who did Beyoncé portray in the movie Cadillac Records?",
+    optA:"BILLIE HOLIDAY",
+    optB:"ARETHA FRANKLIN",
+    optC:"ELLA FITZGERALD",
+    optD:"ETTA JAMES",
+    answer: "ETTA JAMES"
+  },
+  {
+    prompt: "What perfume did Beyoncé develop with Tommy Hilfiger?",
+    optA:"DREAMGIRL",
+    optB:"TRUTH OR DARE",
+    optC:"TRUE STAR",
+    optD:"ENCHANTED",
+    answer: "TRUE STAR"
+  },
+  {
+    prompt: "Who was 'Pretty Hurts' originally written for?",
+    optA:"KATY PERRY",
+    optB:"TAYLOR SWIFT",
+    optC:"JAY-Z",
+    optD:"RIHANNA",
+    answer: "KATY PERRY"
+  },
+  {
+    prompt: "Who does Beyoncé dance with in the music video for 'Naughty Girl'?",
+    optA:"KANYE WEST",
+    optB:"USHER",
+    optC:"MICHAEL EALY",
+    optD:"JAY-Z",
+    answer: "USHER"
+  },
+  {
+    prompt: "Which track earned Beyoncé Song of the Year at the 2010 Grammy Awards?",
+    optA:"BROKEN-HEARTED GIRL",
+    optB:"DIVA",
+    optC:"IF I WERE A BOY",
+    optD:"SINGLE LADIES (PUT A RING ON IT)",
+    answer: "SINGLE LADIES (PUT A RING ON IT)"
+  },
+  {
+    prompt: "Which music video features Jay Z as a passenger in a car speeding along Mission Road in Los Angeles, where he encounters Beyoncé, standing in the middle of the road?",
+    optA:"ME, MYSELF AND I",
+    optB:"NAUGHTY GIRL",
+    optC:"CRAZY IN LOVE",
+    optD:"BABY BOY",
+    answer: "CRAZY IN LOVE"
+  },
+  {
+    prompt: "Destiny's Child got their name from what book of the Bible?",
+    optA:"ISAIAH",
+    optB:"EXODUS",
+    optC:"REVELATIONS",
+    optD:"PSALMS",
+    answer: "ISAIAH"
+  },
+  {
+    prompt: "How many Grammy Awards did Beyoncé earn for her third solo album?",
+    optA:"FOUR",
+    optB:"TWO",
+    optC:"SIX",
+    optD:"ZERO",
+    answer: "SIX"
+  },
+  {
+    prompt: "As a seven-year-old, Beyoncé won a talent show singing what Beatles song?",
+    optA:"LET IT BE",
+    optB:"YESTERDAY",
+    optC:"YELLOW SUBMARINE",
+    optD:"IMAGINE",
+    answer: "IMAGINE"
+  },
+  {
+    prompt: "What was the original name of Destiny's Child?",
+    optA:"PATRON SAINTS",
+    optB:"702",
+    optC:"GIRL'S TYME",
+    optD:"BLAQUE",
+    answer: "GIRL'S TYME"
+  },
+  {
+    prompt: "Beyoncé's younger sister was a back up dancer for what group?",
+    optA:"'N SYNC",
+    optB:"DESTINY'S CHILD",
+    optC:"TLC",
+    optD:"JONAS BROTHERS",
+    answer: "DESTINY'S CHILD"
+  },
+  {
+    prompt: "Where was Beyoncé born?",
+    optA:"AUSTIN",
+    optB:"SAN ANTONIO",
+    optC:"HOUSTON",
+    optD:"DALLAS",
+    answer: "HOUSTON"
+  },
+  {
+    prompt: "What is the name of Beyoncé's fashion line?",
+    optA:"BEY HIVE",
+    optB:"TIDAL",
+    optC:"PAPER CROWN",
+    optD:"HOUSE OF DEREON",
+    answer: "HOUSE OF DEREON"
+  },
+  {
+    prompt: "Who was the manager of Destiny's Child?",
+    optA:"BEYONCE'S MOTHER",
+    optB:"BEYONCE'S BROTHER",
+    optC:"BEYONCE'S FATHER",
+    optD:"BEYONCE",
+    answer: "BEYONCE'S FATHER"
+  },
+  {
+    prompt: "In what song does Beyoncé sing: 'What you wear is all that matters'?",
+    optA:"***FLAWLESS",
+    optB:"PRETTY HURTS",
+    optC:"UPGRADE U",
+    optD:"RUN THE WORLD",
+    answer: "PRETTY HURTS"
+  },
+  {
+    prompt: "What was the name of Beyoncé's second solo album?",
+    optA:"B** DAY",
+    optB:"B-DAY",
+    optC:"B'DAY",
+    optD:"B_DAY",
+    answer: "B'DAY"
+},
 ];
